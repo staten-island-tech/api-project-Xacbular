e01ad31e535845c7d7f8e1d9916a1b39f4c98492
Xacbular <jacobwu2592@gmail.com>
2025-12-22 14:01:25 -0500

    ;)

--- my-project/src/main.js
+++ my-project/src/main.js
@@ -1,3 +1,16 @@
-import "./style.css";
-import javascriptLogo from "./javascript.svg";
-import viteLogo from "/vite.svg";
+// import "./style.css";
+
+const breweryAPI = "https://api.openbrewerydb.org/v1/breweries/random";
+
+async function getData(breweryAPI) {
+  try {
+    const response = await fetch(breweryAPI);
+    const data = await response.json();
+    console.log(data.name);
+    document.getElementById("api-response").innerText = data[0].name;
+    console.log(data);
+  } catch (error) {
+    console.error(error);
+  }
+}
+getData(breweryAPI);
